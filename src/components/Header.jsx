773d9efa3436ce34ed2e480c8cf994230530e460
Hokytsu <igpbrd@gmail.com>
2023-12-07 20:52:08 -0300

    hamburguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../assets/styles/header.css"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 /* Código responsavel pelo cabeçalho*/ 
 
 
 const Header = () => {
+  const [open, setopen] = useState(false);
+
+  const HamburguerOpen= () => {
+    setopen(!open);
+    console.log(open)
+  };
+
     return (
       <header>
         <section id="headerContainer">
@@ -34,7 +42,7 @@ const Header = () => {
           <li className="privateItem">
             <Link to="/menu">MENU_ADM</Link>
           </li>
-         
+        
         </ul>
       </nav>
 
@@ -42,6 +50,11 @@ const Header = () => {
       <div id="container_item">
           <Link to="/login"><button id="btn_login">Log in</button></Link>
           <Link to="/singup"><button id="btn_signup">Sign up</button></Link>
+          <div id="hamburguer" onClick={HamburguerOpen}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
       </div>
       </section>
 
